b75c540c566525a31a6e71e8a301206355063065
vrossi96 <vrossi396@yahoo.it>
2022-03-03 13:48:21 +0100

    add set methods

--- Products.php
+++ Products.php
@@ -7,8 +7,21 @@ class Products
 
    public function __construct($price, $name, $type)
    {
+      $this->setPrice($price);
+      $this->setName($name);
+      $this->setType($type);
+   }
+
+   public function setPrice($price)
+   {
       $this->price = $price;
+   }
+   public function setName($name)
+   {
       $this->name = $name;
+   }
+   public function setType($type)
+   {
       $this->type = $type;
    }
 }
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . '/Products.php';
+
+$scatola = new Products(15, 'Scatoletta di tonno', 'Cibo');
+var_dump($scatola);
